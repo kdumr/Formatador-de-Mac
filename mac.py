c3b5b7a8a29c5d7bd895a52f425d5e08f57f9d68
kdumr <70903649+kdumr@users.noreply.github.com>
2022-12-21 21:54:55 -0300

    Atualização 2.0

--- mac.py
+++ mac.py
@@ -1,10 +1,80 @@
-#NÃO LÊ O CÓDIGO >(
+# NÃO LÊ O CÓDIGO >(
 import pyautogui
 import pyperclip
 import time
 import os
-from tkinter import messagebox
-from pynput.keyboard import Key, Listener, Controller
+import requests
+import tkinter as tk
+import ctypes
+from prettytable import PrettyTable
+from tkinter import messagebox, filedialog
+from pynput.keyboard import Key, Listener
+
+version = 2.0
+nome_do_programa = "mac"
+
+# URL da página que contém informações sobre a versão mais recente
+urlVersao = 'https://raw.githubusercontent.com/kdumr/Formatador-de-Mac/main/versionapp.json'
+
+# URL do arquivo a ser baixado
+urlDownload = 'https://github.com/kdumr/Formatador-de-Mac/raw/main/mac.exe'
+
+# Criar a janela principal
+root = tk.Tk()
+root.withdraw()
+
+table = PrettyTable()
+
+ctypes.windll.kernel32.SetConsoleTitleW("Formatador de MAC")
+# Verificar versão
+try:
+    # Realizar a solicitação HTTP
+    response = requests.get(urlVersao)
+    # Verificar o código de status da resposta
+    if response.status_code == 200:
+        # Se a resposta foi bem-sucedida, obter o conteúdo da resposta
+        content = response.json()
+
+        # Obter a versão mais recente do conteúdo
+        latest_version = version
+
+        # Comparar a versão mais recente com a versão atualmente instalada
+        if latest_version < content:
+            result = messagebox.askyesno(f"Versão atual: {version}", f"Uma nova versão está disponível: {content} \nDeseja instalar?")
+
+            # Verificar o resultado
+            if result:
+                print("Selecione o local que deseja instalar a nova versão...")
+
+                r = requests.get(urlDownload)
+
+                file_path = filedialog.askdirectory()
+
+                open(file_path + f'/{nome_do_programa}.exe', 'wb').write(r.content)
+
+                # Realizar a solicitação HTTP
+                response = requests.get(urlDownload)
+
+                # Verificar o código de status da resposta
+                if response.status_code == 200:
+                    # Se a resposta foi bem-sucedida, obter o conteúdo da resposta
+                    content = response.content
+
+                    tk.messagebox.showinfo("Sucesso!", "Uma nova versão do aplicativo foi instalada com sucesso!")
+                    exit()
+
+                else:
+                    print('Erro ao baixar o arquivo:', response.status_code)
+            else:
+                table.field_names = ["ATENÇÃO!"]
+                table.add_row(["Você está usando uma versão antiga do aplicativo!"])
+                print(table)
+        else:
+            print('-> Você está usando a versão mais recente.', version)
+    else:
+        print('Erro ao verificar a versão mais recente:', response.status_code)
+except requests.exceptions.ConnectionError:
+    print("-> Não foi possível verificar novas versões do aplicativo, parece que você está sem conexão.")
 
 
 def inicio():
@@ -13,19 +83,23 @@ def inicio():
     print("Use a tecla END para colar o mac no Flashman")
     print("============================================\n")
 
+
 lista = []
 continuar = True
+
+
 def show(key):
     if key == Key.end:
         for i in range(len(lista)):
             pyperclip.copy(lista[i])
             pyautogui.hotkey('ctrl', 'v')
             pyautogui.hotkey('enter')
-            time.sleep(1) 
+            time.sleep(1)
         print('OS MACS FORAM COLADOS')
         return False
     if key == Key.delete:
         return False
+
 
 inicio()
 while continuar:
@@ -41,7 +115,7 @@ while continuar:
         print("")
         print("Use a tecla DELETE para continuar digitando os macs")
         print("============================================\n")
-        with Listener(on_press = show) as listener:
+        with Listener(on_press=show) as listener:
             listener.join()
     elif numIn == "1":
         os.system('cls') or None
@@ -66,6 +140,3 @@ while continuar:
         mac = '{}:{}:{}:{}:{}:{}'.format(numIn[:2], numIn[2:4], numIn[4:6], numIn[6:8], numIn[8:10], numIn[10:])
         print(f"MAC: {mac}\n")
         lista.append(mac)
-    
-
-
