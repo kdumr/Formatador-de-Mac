aa987649310ebfa9050c440c10264ec10c70e11e
kdumr <70903649+kdumr@users.noreply.github.com>
2023-03-27 12:53:49 -0300

    Add files via upload

--- mac.py
+++ mac.py
@@ -10,6 +10,7 @@ import datetime
 import traceback
 import colorama
 import pynput
+from tkinter import messagebox
 from prettytable import PrettyTable
 from pynput.keyboard import Key, Listener
 from conection import *
@@ -28,7 +29,7 @@ titulo = f"""{colorTexto.YELLOW}
 |____/ \___/|_|\__,_|\__\___|\___|
 {colorTexto.RESET}
 """
-version = 2.8
+version = 3.2
 nome_do_programa = "mac"
 
 # URL da página que contém informações sobre a versão mais recente
@@ -42,6 +43,7 @@ root = tk.Tk()
 root.withdraw()
 
 table = PrettyTable()
+
 
 # Modifica o nome do CMD
 ctypes.windll.kernel32.SetConsoleTitleW("Formatador de MAC")
@@ -63,9 +65,58 @@ class Main:
                 print(cor + "| " + linha.ljust(tamanho) + " |")
             print(cor + linha_superior + colorTexto.RESET)
 
-
+        def cancelar(key):
+            if key == Key.home:
+                return False
+            
         def show(key):
+            if key == Key.home:
+                # Bloquear Mouse
+                i = 0
+                while True:
+                    pyperclip.copy(lista[i])
+                    pyautogui.hotkey('ctrl', 'v')
+                    pyautogui.hotkey('enter')
+                    i += 1
+                    if i == len(lista):
+                        break
+                    else:
+                        with Listener(on_press=cancelar) as listener:
+                            listener.join()
+
+                os.system('cls') or None
+                print(titulo)
+                caixaTexto((" " * 10 + "OS MACS FORAM COLADOS" + " " * 10), colorTexto.GREEN)
+                print("")
+                return False
+                
             if key == Key.end:
+                # Bloquear Mouse
+                original_position = pyautogui.position()
+                pyautogui.FAILSAFE = False
+                pyautogui.moveTo(-100, -100)
+                mouse_listener = pynput.mouse.Listener(suppress=True)
+                mouse_listener.start()
+                for i in range(len(lista)):
+                    pyperclip.copy(lista[i])
+                    time.sleep(0.5)
+                    pyautogui.hotkey('ctrl', 'v')
+                    pyautogui.hotkey(',')
+                pyautogui.hotkey('enter')
+                os.system('cls') or None
+                print(titulo)
+                caixaTexto((" " * 10 + "OS MACS FORAM COLADOS" + " " * 10), colorTexto.GREEN)
+                print("")
+                # Desbloquear Mouse
+                mouse_listener.stop()
+                pyautogui.moveTo(original_position)
+                pyautogui.FAILSAFE = True
+                return False
+            
+            if key == Key.delete:
+                return False
+            
+            if key == Key.page_up:
                 # Bloquear Mouse
                 original_position = pyautogui.position()
                 pyautogui.FAILSAFE = False
@@ -81,13 +132,10 @@ class Main:
                 print(titulo)
                 caixaTexto((" " * 10 + "OS MACS FORAM COLADOS" + " " * 10), colorTexto.GREEN)
                 print("")
-
                 # Desbloquear Mouse
                 mouse_listener.stop()
                 pyautogui.moveTo(original_position)
                 pyautogui.FAILSAFE = True
-                return False
-            if key == Key.delete:
                 return False
 
         print(titulo)
@@ -104,7 +152,7 @@ class Main:
             elif numIn == "0":
                 os.system('cls') or None
                 print(titulo)
-                caixaTexto("Use a tecla [END] para colar o mac no Flashman\n\nUse a tecla [DELETE] para continuar digitando os macs")
+                caixaTexto(f'Use a tecla {colorTexto.LIGHTGREEN_EX}[END]{colorTexto.RESET} para colar todos os macs em sequência\n\nUse a tecla {colorTexto.GREEN}[HOME]{colorTexto.RESET} para colar UM mac por vez\n\nUse a tecla {colorTexto.CYAN}[PAGE UP]{colorTexto.RESET} para colar os macs na vertical\n\nUse a tecla {colorTexto.LIGHTRED_EX}[DELETE]{colorTexto.RESET} para continuar digitando os macs')
                 with Listener(on_press=show) as listener:
                     listener.join()
             elif numIn == "1":
@@ -129,7 +177,7 @@ class Main:
                     for item in lista:
                         print(colorTexto.MAGENTA + item + "\n")
             elif len(numIn) < 12 or len(numIn) > 12:
-                tk.messagebox.showerror("Erro", "O número digitado é inválido!")
+                caixaTexto("ERRO: O NÚMERO DIGITADO É INVÁLIDO", colorTexto.RED, "=")
             elif numIn == "info":
                 os.system('cls') or None
                 print("Informações do aplicativo:")
@@ -139,9 +187,12 @@ class Main:
 
             else:
                 mac = '{}:{}:{}:{}:{}:{}'.format(numIn[:2], numIn[2:4], numIn[4:6], numIn[6:8], numIn[8:10], numIn[10:])
-                caixaTexto(f"MAC: {mac}")
-                print("")
-                lista.append(mac)
+                if mac in lista:
+                    caixaTexto("ATENÇÃO: ESTE MAC JÁ EXISTE NA LISTA", colorTexto.YELLOW, "=")
+                else:
+                    caixaTexto(f"MAC: {mac}")
+                    print("")
+                    lista.append(mac)
 
 if __name__ == "__main__":
     try:
